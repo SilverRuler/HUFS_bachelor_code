277de390a96271ede7a64830fbada39c8045a18b
SilverRuler <tyeun7@gmail.com>
2025-04-20 18:58:48 +0900

    mm

---
+++ ex/birthunse.php
@@ -0,0 +1,80 @@
+﻿<html>
+<head>
+<title>생년월일 운세</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+
+<script language="javascript">
+
+function check_jumin() { 
+
+ var jumin=document.getElementById('jumin1').value + document.getElementById('jumin2').value;
+  var fmt = /^\d{6}[1234]\d{6}$/;  //포맷 설정 ,양식체크 , regular expression 패턴임.
+  if (!fmt.test(jumin)) {
+      return false;
+  }
+  var birthYear = (jumin.charAt(6) <= "2") ? "19" : "20";
+  birthYear += jumin.substr(0, 2);
+  var birthMonth = jumin.substr(2, 2) - 1;
+  var birthDate = jumin.substr(4, 2);
+  var birth = new Date(birthYear, birthMonth, birthDate);
+
+  if ( birth.getYear() % 100 != jumin.substr(0, 2) ||
+       birth.getMonth() != birthMonth ||
+       birth.getDate() != birthDate) {
+     return false;
+  }
+
+
+  // Check Sum 코드의 유효성 검사
+  var buf = new Array(13);
+  for (var i = 0; i < 13; i++) buf[i] = parseInt(jumin.charAt(i));
+ 
+  multipliers = [2,3,4,5,6,7,8,9,2,3,4,5];
+  for (var sum = 0, i = 0; i < 12; i++) sum += (buf[i] *= multipliers[i]);
+
+  if ((11 - (sum % 11)) % 10 != buf[12]) {
+     return false;
+  }
+
+  
+  return true;
+}
+
+
+function checks(){
+ if(check_jumin())
+  alert("구축중입니다...");
+
+ else
+  alert("생년월일 주민번호가 올바르지 않습니다.");
+}
+
+
+
+function nextgo(e){  
+  if (e.value.length>=6) {
+   document.getElementById('jumin2').focus();
+  }
+}
+
+
+
+</script>
+</head>
+
+
+<body>
+<?php
+echo "<center> <h1> 운세를 점칠 생년월일을 기입하세요.</h1></center>";
+?>
+
+
+
+<input type="text" id="jumin1" name="jumin1" onKeyUp="nextgo(this);"/> 
+-<input type="password" id="jumin2" name="jumin2" />
+
+<input type="button" onClick="checks()" value="운세보기"/>
+
+</body>
+</html>
---
+++ ex/index.php
@@ -0,0 +1,198 @@
+﻿<HTML>  
+<HEAD>
+	<TITLE>201104136_멀티미디어시스템_과제1</TITLE>
+</HEAD>
+
+<script>
+function handleMouseOver(e) {
+    //e.target.style.color='blue';
+    e.style.color='blue';
+}
+
+function hideVisibility()
+{
+  var tag = document.getElementById("mm");
+
+  if(tag.style.visibility == "visible")
+     tag.style.visibility =  "hidden";
+  else tag.style.visibility =  "visible"
+}
+
+</script>
+
+
+<BODY>
+
+<center>
+<IMG src=/todayunse.jpg border=1 width=680 height=380 usemap=#m1>
+<map name=m1>
+<area href=/index.php shape=rect coords="250, 40, 300, 90" alt=“홈페이지”>
+</map>
+</center>
+
+
+<center>
+<p> 호랑이를 누르면 홈페이지로 이동합니다 <br>
+<p> 자 축 인 묘 진 사 오 미 신 유 술 해 <br>
+<p> 쥐 소 호 토 원 백 말 양 원 닭 개 돼 <br>
+</center>
+
+<marquee behavior=scroll  direction=right bgcolor=#8855FF width=600 height=20> 201104136 멀티미디어 과제 1 </marquee> 
+
+<hr>
+<div style="cursor:hand; color:blue;"    
+     onClick="hideVisibility()"
+     onMouseOver="handleMouseOver(this)" 
+     onMouseOut="this.style.color='black'"
+     title="Click!!"> 오늘의 운세  보기/숨기기 </div>
+     <div id=mm style="overflow:scroll; white-space:wrap; width:700px;
+ height:100px; padding:10px; background-color:gold; visibility:">
+
+▶쥐띠= 농담이 와전되어 구설에 오를 수. 사랑에 마음의 표시를 할 때다. ㄱㅊㅍ성씨 애정에도 멍들고 자식걱정으로 한숨만 느는 격. 언행조심. 6, 7, 9월생 휴일의 인연이 이어져 사랑의 동반자를 만나거나 인생의 귀인을 만날 듯. 흰색이 길.
+
+▶소띠= 남을 의지하려다 사탕발림에 현혹되어 곤경에 빠질 운. 현재는 힘에 겨우나 자신있게 실력으로 추진하면 승산이 있을 듯. 73년 1, 2, 11월생 마음에서 떠난 사람 미련 두지 말라. 1, 2, 11월생 개척이 급선무. 잡념을 버리면 동·북쪽에 행운 있다.
+
+▶범띠= 건강으로 인해 화목한 가정에 우환이 닥칠 수. 각별히 건강에 조심하고 외출은 삼감이 좋을 듯. 4, 9, 12월생 기름진 것이나 회 종류의 음식은 피할 것. 86년생 ㅇㅊㅂ성씨 아기의 잉태를 보려면 아내만 구박 말고 자신도 병원의 문을 두드려라.
+
+▶토끼띠= 상사를 무시하면 자신도 존경받지 못함을 알아야 할 떄다. 자만심과 우월감은 금물. ㅂㅈㅊ성씨 겸손한 태도로 자기 일에 충실해야 매사가 순조롭게 풀리는 운이다. 5, 9, 10월생 새로운 사업으로 변동해도 무방할 듯. 돼지, 용, 양띠의 도움을 받으면 금상첨화. 
+
+▶용띠= 벌여놓은 살림이 신경 쓰이는 날이다. 4, 5, 6월생 서쪽 사업에 바람이 불고 북쪽 사람이 잔소리 하는 날. 근신하고 겸손하게 행동하라. 모든게 당신의 과욕 탓임을 알 것. 여자는 감언이설 조심. 76년생 생일 일자가 짝수인 여성은 올해 배필을 만날 운.
+
+▶뱀띠= 허영심으로 뜬구름 잡는 자만심을 버려라. 가정에서 외면당하고 자신도 초라해질 운. ㅇㅊㅂ성씨 금전거래하다 관재수 있겠으니 4, 7, 11월생과는 거래를 자중할 것. 힘든 일은 제일 가까운 개, 닭띠와 의논하면 풀리는 길이 열릴 듯.
+
+▶말띠= 여행길에 만난 친구의 도움으로 좋은 인연을 만날 운. 이색적인 당신의 성격에 호감을 가지는구나. 1, 8, 9월생 파란색, 남색 계통의 의복을 입고 동향으로 데이트를 감도 좋을 듯. 66년생 ㅈㅂㅎ 성씨 진정한 자세로 봉사활동을 생활화함도 좋겠다.
+
+▶양띠= 날고 싶은 새를 잡아두는 격. 자식이 원하는 길에 힘이 되라. 경험은 승자가 되는 지름길. ㄱㅊㅍ성씨 사업가는 소득이 있을 운이니 철저한 자기관리가 필요할 때. 2, 3, 10월생 서남쪽 이사는 신병이 따를 수니 피할 것. 애정은 현명한 판단 요구됨.
+
+▶원숭이띠= 작심삼일 말부터 앞세우는 행위는 득이 없는 법. 행동으로 실천하라. 1, 2, 12월생 진행 중인 사업에 자신을 갖고 부부의 뜻이 일치하면 성공운이 높을 듯. 어제는 사장이요 오늘은 종업원이 되었어도 웃음꽃 필 수. 자녀에게 관심 가져야 할 때.
+
+▶닭띠= 강하다는 표현은 당신을 두고 한 말. 어떠한 난관도 해쳐나갈 수 있는 능력자지만 5, 6, 11월생 신경성으로 실수를 초래할 때가 있겠다. 적극적으로 꾸준한 전진만이 성공의 지름길. 69년생 ㄱㅂㅊ성씨 법적 절차 밟지 않으면 갈림수 있을 듯.
+
+▶개띠= 마음만 착하면 자칫 어리숙하게 보일 수 있으니 재치와 지혜를 보강함이 방패다. 그러면 인정받는 재능인이 될 복운. 2, 6, 9월생 외국진출의 꿈 성사될 듯. 부부의 눈치작전 이제 그만. 근심걱정의 원인. 대화로 풀고 자식의 마음 헤아릴 것.
+
+▶돼지띠= 정직하고 부지런한 당신이나 결단력이 부족하여 행운도 놓칠 우려 있다. 마음을 비우고 겸허하게 행동하라. 3, 5, 9월생 건설, 스포츠, 목재업 종사자는 서서히 풀릴 운. 눈가리고 아웅은 말 것. 옷감도 중요하지만 바느질의 튼튼함도 옷의 가격을 좌우함. 
+
+</div>
+
+
+
+
+<hr>
+<FONT size=6 color=blue>운세의 종류</FONT>
+
+
+
+<hr>
+<p><p>
+<OL type=A>
+		<LI> <a href = /birthunse.html>생년월일 운세 </a>
+		<LI> <a href = /ddiunse.html>띠별 운세</a>
+        <UL type=circle>
+        	<LI>쥐띠
+            <LI>소띠
+            <LI>호랑이띠
+            <LI>토끼띠
+            <LI>용띠
+            <LI>뱀띠
+            <LI>말띠
+            <LI>양띠
+            <LI>원숭이띠
+            <LI>닭띠
+            <LI>개띠
+            <LI>돼지띠
+        </UL>
+        <LI> <a href = /bjrunse.html>별자리 운세</a>
+		<UL type=square>
+			<LI> 물병자리
+			<LI> 물고기자리
+			<LI> 양자리
+			<LI> 황소자리
+            <LI> 쌍둥이자리
+            <LI> 게자리
+            <LI> 사자자리
+            <LI> 처녀자리
+            <LI> 천칭자리
+            <LI> 전갈자리
+            <LI> 사수자리
+            <LI> 염소자리
+		</UL>
+</OL>
+<hr>
+
+
+<p>
+<center> 간단한 운세 영상 </center>
+<p>
+<embed src=tarom.mp4 loop=true autostart=false  width=360px height=240px>
+
+<hr>
+<p>
+
+<a name=place1>
+
+<center> 운세 전문가 소개 </center>
+<p>
+<TABLE border=1 bgcolor=#AAAACC cellpadding=5px cellspacing=0px>
+
+		<TR>
+			<TH>얼굴</TH>
+			<TH>이름</TH>
+			<TH>연락처</TH>
+		</TR>
+		<TR>
+			<TD><IMG src=/gang.jpg border=1 width=200 height=200>
+            </TD>
+			<TD>강호동님</TD>
+			<TD>1588-2000</TD>
+		</TR>
+		<TR>
+			<TD><IMG src=/lee.jpg border=1 width=200 height=200>
+</TD>
+			<TD>이수근님</TD>
+			<TD>1588-2001</TD>
+		</TR>
+</TABLE>
+
+<hr>
+<p>
+<center> 그밖의 운세 </center>
+<p>
+<TABLE border=1 bgcolor=#CCCCAA cellpadding=5px cellspacing=0px>
+
+		<TR>
+			<TH colspan=2 align = center>또다른 운수 종류</TH>
+		</TR>
+		<TR>
+			<TD><IMG src=/taro.jpg border=1 width=200 height=200>
+            </TD>
+			<TD>타로운세</TD>
+		</TR>
+		<TR>
+			<TD><IMG src=/hatu.jpg border=1 width=200 height=200>
+</TD>
+			<TD>화투운세</TD>
+		</TR>
+</TABLE>
+<p>
+
+<hr>
+<p>
+<center> 운세 참고 자료 </center>
+<p>
+
+<div>참고 자료 및 appendix</div>
+
+<A href=http://www.fortune82.com/orient_main.asp>인터넷점집 포춘82</A>
+<br>
+<A target=_blank href=https://search.naver.com/search.naver?where=nexearch&ie=utf8&sm=tab_etc&query=%EC%83%9D%EB%85%84%EC%9B%94%EC%9D%BC%20%EC%9A%B4%EC%84%B8>네이버 운세</A>
+<br>
+<A href=#place1>운세 전문가 연락처 </A>
+<br>
+<A href=mailto:tyeun7@naver.com>관리자 메일 보내기</A>
+<br>
+
+
+
+
+</BODY>
+</HTML>
